f22b63e1fa5da57f02dd8d2c94dc7fb0ef5370a2
puspaningtyas <puspaningtyas@gmail.com>
2026-02-27 03:08:10 +0700

    buat project baru maven

---
+++ SDL_2/src/test/java/com/test/LectureJUnitTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author puspa
+ */
+public class LectureJUnitTest {
+    
+    public LectureJUnitTest() {
+    }
+
+    
+}
