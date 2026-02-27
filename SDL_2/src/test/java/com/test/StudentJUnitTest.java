7ed66d2fc490e6b6f129a3130ed80adab80c0454
puspaningtyas <puspaningtyas@gmail.com>
2026-02-27 09:16:34 +0700

    add test Student

---
+++ SDL_2/src/test/java/com/test/StudentJUnitTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author puspa
+ */
+public class StudentJUnitTest {
+    
+    public StudentJUnitTest() {
+    }
+    
+    @Test
+    public void testLecture1() {
+        //test 1
+        Student test = new Student("1","Budi");
+        assertEquals("1",test.getId());
+        assertNotEquals("2",test.getId());
+    }
+    
+    @Test
+    public void testLecture2() {
+        //test 1
+        Student test = new Student("1","Puspa");
+        assertEquals("Puspa",test.getName());
+        assertNotEquals("not Puspa",test.getName());
+    }
+}
