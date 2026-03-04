a0d1148760676c22a5c8921e3421751900f27a10
user <user@DESKTOP-IEPAACU>
2026-03-04 17:01:32 +0700

    update MyStudentLarik

--- SDL_2/src/main/java/com/sdl/MyStudentLarik.java
+++ SDL_2/src/main/java/com/sdl/MyStudentLarik.java
@@ -10,5 +10,73 @@ package com.sdl;
  * @author puspa
  */
 public class MyStudentLarik {
+    // size/panjang larik dengan tipe data int
+    private int size; // ukuran larik
+    // larik penyimpan data bertipe Student
+    private Student[] data; // larik penyimpan data
+
+    /**
+     * Konstruktor
+     */
+    public MyStudentLarik() {
+    }
+
+    /**
+     * Konstruktor dengan parameter masukan ukuran data
+     * @param size 
+     */
+    public MyStudentLarik(int size) {
+        this.size = size;
+        this.data = new Student[this.size];
+    }
+
+    /**
+     * Konstruktor dengan masukan data larik
+     * @param data 
+     */
+    public MyStudentLarik(Student[] data) {
+        this.data = data;
+        this.size = data.length;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the data
+     */
+    public Student[] getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Student[] data) {
+        this.data = data;
+    }
     
+    public void append(int indeks,Student value){
+        this.data[indeks]=value;
+    }
+    
+    /**
+     * Fungsi untuk mengambil nilai dari larik data sesuai posisi masukan indeks.
+     * @param indeks
+     * @return 
+     */
+    public Student getValue(int indeks){
+        return this.data[indeks];
+    }
 }
