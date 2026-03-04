11c895b5d9311b2d57c7e41813e65217c3305b49
puspaningtyas <puspaningtyas@gmail.com>
2026-03-04 09:48:30 +0700

    tambah larik test deret hitung

--- SDL_2/src/test/java/com/test/MyLarikJUnitTest1.java
+++ SDL_2/src/test/java/com/test/MyLarikJUnitTest1.java
@@ -8,6 +8,7 @@ package com.test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import com.sdl.MyLarik;
 
 /**
  *
@@ -20,6 +21,9 @@ public class MyLarikJUnitTest1 {
     
     @Test
     public void testMyLarik() {
-    
+       int[] data = MyLarik.getSeries(0, 5, 2);
+       assertEquals(0,data[0]);
+       assertEquals(2,data[1]);
+       assertNotEquals(2,data[0]);
     }
 }
