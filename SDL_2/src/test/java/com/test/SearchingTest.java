c85a214c98aed52039a759dd6ec0124c654d4929
puspaningtyas <puspaningtyas@gmail.com>
2026-03-12 14:48:25 +0700

    update SearchingTest ini bagian modul Searching

---
+++ SDL_2/src/test/java/com/test/SearchingTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.sdl.*;
+
+/**
+ *
+ * @author puspa
+ */
+public class SearchingTest {
+    
+    public SearchingTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @Test
+    public void testCetakArray(){
+        double[] data = {5,4,1,10,21,15,13};
+        MyLarik test = new MyLarik(data);
+        test.cetakData();
+        
+        double[] data1 = {55,4,1,100,2,15,13};
+        MyLarik test1 = new MyLarik(data1);
+        test1.cetakData();
+    }
+    @Test
+    public void testSequential(){
+        double[] data = {5,4,1,10,21,15,13};
+        MyLarik test = new MyLarik(data);
+        assertEquals(0,test.sequentialSearch(5));
+        assertEquals(-1,test.sequentialSearch(100));
+        
+        double[] data1 = {55,4,1,100,2,15,13};
+        MyLarik test1 = new MyLarik(data1);
+        assertEquals(1,test.sequentialSearch(4));
+        assertEquals(-1,test.sequentialSearch(20));
+    }
+    
+    @Test
+    public void testBinary(){
+        double[] data = {5,4,1,10,21,15,13};
+        MyLarik test = new MyLarik(data);
+        assertEquals(0,test.binarySearch(5));
+        assertEquals(-1,test.binarySearch(100));
+        
+        double[] data1 = {55,4,1,100,2,15,13};
+        MyLarik test1 = new MyLarik(data1);
+        assertEquals(1,test.binarySearch(4));
+        assertEquals(-1,test.binarySearch(20));
+    }
+    
+    @Test
+    public void testInterpolation(){
+        double[] data = {5,4,1,10,21,15,13};
+        MyLarik test = new MyLarik(data);
+        assertEquals(0,test.interpolationSearch(5));
+        assertEquals(-1,test.interpolationSearch(100));
+        
+        double[] data1 = {55,4,1,100,2,15,13};
+        MyLarik test1 = new MyLarik(data1);
+        assertEquals(1,test.interpolationSearch(4));
+        assertEquals(-1,test.interpolationSearch(20));
+    }
+}
