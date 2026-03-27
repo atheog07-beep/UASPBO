638dadb5d75a29ccf4f75feda8462b96068f1e02
puspaningtyas <puspaningtyas@gmail.com>
2026-03-27 14:05:03 +0700

    update SortTest getMaxData()

---
+++ SDL_2/src/test/java/com/test/sort/SortTest2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test.sort;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.sdl.*;
+/**
+ *
+ * @author puspa
+ */
+public class SortTest2 {
+    
+    public SortTest2() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @Test
+    public void sortTest(){
+        double[] data = {5,4,1,10,21,15,13};
+        int maxIndeks = MyLarik.getMaxData(data);
+        assertEquals(4,maxIndeks);
+    }
+}
