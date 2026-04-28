b4d53628fd27b49c68f7fd3e66cb09580441b779
puspaningtyas <puspaningtyas@gmail.com>
2026-04-28 13:49:54 +0700

    buat kelas MyNode

---
+++ SDL_2/src/main/java/com/sdl/list/MyNode.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sdl.list;
+
+import com.sdl.latihan.MyItem;
+
+/**
+ *
+ * @author puspa
+ */
+public class MyNode extends MyItem{
+    private MyNode next; // link ke sesama obyek MyNode
+
+    /**
+     * Konstruktor
+     */
+    public MyNode() {
+    }
+    
+    
+}
