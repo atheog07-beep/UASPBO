5e66f00f301019fa7ef07181a84adc9218a07b03
asus <asus@LAURA-ANGELINA>
2026-05-18 21:16:09 +0700

    First Commit

--- src/percobaanuas/Pasien.java
+++ src/percobaanuas/Pasien.java
@@ -10,4 +10,85 @@ package percobaanuas;
  */
 public class Pasien {
     
+    private String NIK;
+    private String Nama;
+    private int Umur;
+    private String Alamat;
+    private int JenisKelamin;
+    private String Keluhan;
+    private int NomorAntrian;
+
+    public Pasien(String NIK, String Nama, int Umur, String Alamat, int JenisKelamin, String Keluhan, int NomorAntrian) {
+        this.NIK = NIK;
+        this.Nama = Nama;
+        this.Umur = Umur;
+        this.Alamat = Alamat;
+        this.JenisKelamin = JenisKelamin;
+        this.Keluhan = Keluhan;
+        this.NomorAntrian = NomorAntrian;
+    }
+
+    public String getNIK() {
+        return NIK;
+    }
+
+    public void setNIK(String NIK) {
+        this.NIK = NIK;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public int getUmur() {
+        return Umur;
+    }
+
+    public void setUmur(int Umur) {
+        this.Umur = Umur;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String Alamat) {
+        this.Alamat = Alamat;
+    }
+
+    public int getJenisKelamin() {
+        return JenisKelamin;
+    }
+
+    public void setJenisKelamin(int JenisKelamin) {
+        this.JenisKelamin = JenisKelamin;
+    }
+
+    public String getKeluhan() {
+        return Keluhan;
+    }
+
+    public void setKeluhan(String Keluhan) {
+        this.Keluhan = Keluhan;
+    }
+
+    public int getNomorAntrian() {
+        return NomorAntrian;
+    }
+
+    public void setNomorAntrian(int NomorAntrian) {
+        this.NomorAntrian = NomorAntrian;
+    }
+    
+    public void Daftar(int NomorAntrian){
+}
+    public String toString(){
+    
+        return null;
+    
+    }
 }
