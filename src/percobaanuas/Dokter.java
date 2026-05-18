45c41489db7ea976e5b35b2087c7f8f557e72d45
asus <asus@LAURA-ANGELINA>
2026-05-18 21:14:05 +0700

    First Commit

--- src/percobaanuas/Dokter.java
+++ src/percobaanuas/Dokter.java
@@ -8,6 +8,60 @@ package percobaanuas;
  *
  * @author ath07
  */
-public class Dokter {
+public abstract class Dokter {
+    
+    private String NIP;
+    private String Nama;
+    private String Jadwal;
+    
+    protected double TarifKonsultasi;
+
+    public Dokter(String NIP, String Nama, String Jadwal, double TarifKonsultasi) {
+        this.NIP = NIP;
+        this.Nama = Nama;
+        this.Jadwal = Jadwal;
+        this.TarifKonsultasi = TarifKonsultasi;
+    }
+
+    public String getNIP() {
+        return NIP;
+    }
+
+    public void setNIP(String NIP) {
+        this.NIP = NIP;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public String getJadwal() {
+        return Jadwal;
+    }
+
+    public void setJadwal(String Jadwal) {
+        this.Jadwal = Jadwal;
+    }
+
+    public double getTarifKonsultasi() {
+        return TarifKonsultasi;
+    }
+
+    public void setTarifKonsultasi(double TarifKonsultasi) {
+        this.TarifKonsultasi = TarifKonsultasi;
+    }
+    
+    public abstract void Periksa(Pasien pasien);
+    
+    public String getInfo(){
+        return "";
+    }
+    
+    
+    
     
 }
