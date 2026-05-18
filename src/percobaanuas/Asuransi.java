561c14d512422aee833204ee0adb2ebe37cfe49e
asus <asus@LAURA-ANGELINA>
2026-05-18 21:12:44 +0700

    First Commit

--- src/percobaanuas/Asuransi.java
+++ src/percobaanuas/Asuransi.java
@@ -9,6 +9,94 @@ package percobaanuas;
  *
  * @author ath07
  */
-public class Asuransi {
+
+public class Asuransi implements Pembayaran {
+
+    // ---------- Atribut ----------
     
+    private String nomorAsuransi;
+    private String namaPerusahaan;
+    private String jenisAsuransi;
+    private double totalBayar;
+    private double potongan;
+
+    
+    // ---------- Constructor ----------
+    
+    public Asuransi(String nomorAsuransi, String namaPerusahaan,
+            String jenisAsuransi, double totalBayar, double potongan) {
+
+        this.nomorAsuransi = nomorAsuransi;
+        this.namaPerusahaan = namaPerusahaan;
+        this.jenisAsuransi = jenisAsuransi;
+        this.totalBayar = totalBayar;
+        this.potongan = potongan;
+    }
+
+    
+    // ---------- Getter & Setter ----------
+    
+    public String getNomorAsuransi() {
+        return nomorAsuransi;
+    }
+
+    public void setNomorAsuransi(String nomorAsuransi) {
+        this.nomorAsuransi = nomorAsuransi;
+    }
+
+    public String getNamaPerusahaan() {
+        return namaPerusahaan;
+    }
+
+    public void setNamaPerusahaan(String namaPerusahaan) {
+        this.namaPerusahaan = namaPerusahaan;
+    }
+
+    public String getJenisAsuransi() {
+        return jenisAsuransi;
+    }
+
+    public void setJenisAsuransi(String jenisAsuransi) {
+        this.jenisAsuransi = jenisAsuransi;
+    }
+
+    public double getTotalBayar() {
+        return totalBayar;
+    }
+
+    public void setTotalBayar(double totalBayar) {
+        this.totalBayar = totalBayar;
+    }
+
+    public double getPotongan() {
+        return potongan;
+    }
+
+    public void setPotongan(double potongan) {
+        this.potongan = potongan;
+    }
+
+    
+    // ---------- Method ----------
+    
+    public double hitungTagihan() {
+        return totalBayar - potongan;
+    }
+
+    @Override
+    public void Bayar() {
+
+    }
+
+    @Override
+    public String Cetak() {
+        return "Pembayaran Asuransi";
+    }
+
+    @Override
+    public double Total() {
+        return hitungTagihan();
+    }
 }
+    
+
