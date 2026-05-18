4b5565487a7563b4a1030df1494e3995556f5750
asus <asus@LAURA-ANGELINA>
2026-05-18 21:15:25 +0700

    First Commit

--- src/percobaanuas/DokterUmum.java
+++ src/percobaanuas/DokterUmum.java
@@ -8,6 +8,29 @@ package percobaanuas;
  *
  * @author ath07
  */
-public class DokterUmum {
+public class DokterUmum extends Dokter {
+    
+    private String Poliklinik;
+    private final int JumlahMaksPasien = 80;
+    private int Antrian;
+
+    public DokterUmum(String Poliklinik, int Antrian, String NIP, String Nama, String Jadwal, double TarifKonsultasi) {
+        super(NIP, Nama, Jadwal, TarifKonsultasi);
+        this.Poliklinik = Poliklinik;
+        this.Antrian = Antrian;
+    }
+    @Override
+    public void Periksa(Pasien pasien) {
+    }
+    
+    public String Diagnosa(){
+        return "";
+    }
+    
+    @Override
+     public String getInfo(){
+        return "";
+    }
+    
     
 }
