761ef3037c5dc05e3a637dd68229a18df9c9030b
atheog07-beep <atheog07@gmail.com>
2026-06-12 12:39:11 +0700

    edit medical record

--- src/percobaanuas/MedicalRecord.java
+++ src/percobaanuas/MedicalRecord.java
@@ -9,16 +9,36 @@ public class MedicalRecord {
 
     public MedicalRecord(Pasien data) {
         this.data = data;
+        this.riwayatPemeriksaan = new Pemeriksaan[50];
+        this.jmlRiwayat = 0;
     }
 
     public Pasien getData() {
         return data;
     }
     public void tambahRiwayat(Pemeriksaan p) {
-        
+        if (jmlRiwayat < riwayatPemeriksaan.length) {
+            riwayatPemeriksaan[jmlRiwayat] = p;
+            jmlRiwayat++;
+        }
     }
     public void cetakRiwayat() {
+        System.out.println("------- RIWAYAT MEDICAL RECORD ------");
+        System.out.println("Pasien : " + data.getNama() + " (" + data.getNMR() + ")");
+        if (jmlRiwayat == 0) {
+            System.out.println("Belum ada riwayat pemeriksaan.");
+        } else {
+            for (int i = 0; i < jmlRiwayat; i++) {
+                System.out.println("\n[Kunjungan " + (i+1) + "]");
+                riwayatPemeriksaan[i].CetakData();
+                if (riwayatPemeriksaan[i].getResep() != null) {
+                    riwayatPemeriksaan[i].getResep().cetakResep();
+                }
+            }
+        }
+        System.out.println("==================================");
     }
     
-    
+    public int getJmlRiwayat() { return jmlRiwayat; }
+    public Pemeriksaan[] getRiwayatPemeriksaan() { return riwayatPemeriksaan; }
 }
